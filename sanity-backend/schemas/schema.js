153a38d77bff11b991ee0423c1a7664b1096cab4
brickly26 <sabburak26@gmail.com>
2022-08-07 12:11:56 -0400

    added sanity backend aswell as installing dependencies

--- sanity-backend/schemas/schema.js
+++ sanity-backend/schemas/schema.js
@@ -3,8 +3,6 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import user from './user';
-import like from './like';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,6 +12,5 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    like, user
   ]),
 })
