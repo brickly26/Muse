0581a9baa53b8c1072fed6089ae3272bcda6a806
brickly26 <sabburak26@gmail.com>
2022-08-07 12:27:58 -0400

    added my first components navbar/sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Navbar from '../components/Navbar'
+import '../styles/globals.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
