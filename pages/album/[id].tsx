6ed438e1f76e3865434ff0c08144ab1fa0911a46
brickly26 <sabburak26@gmail.com>
2022-08-26 11:58:48 -0400

    ...

--- pages/album/[id].tsx
+++ pages/album/[id].tsx
@@ -69,7 +69,6 @@ const Album = ({ albumDetails }: IProps) => {
                   width={200}
                   height={200}
                   className="rounded-md"
-                  alt={albumDetails.name}
                 />
               </div>
               <div className="flex flex-col justify-around flex-1 w-full">
